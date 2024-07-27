09729ac3e4b208a44560e0d25104b228ddeb333a
salagata <122649005+salagata@users.noreply.github.com>
2024-07-27 17:02:43 -0500

    Added Custom Params

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const main = document.getElementById("tables");
+const urlParams = new URLSearchParams(window.location.search);
+const [sta,end,ste,bse] = [urlParams.get("start"),urlParams.get("end"),urlParams.get("step"),urlParams.get("base")]
 
 function generateRow(st,en,sp,base) {
     let multiTable = ""
@@ -6,12 +8,12 @@ function generateRow(st,en,sp,base) {
     for (let i = st; i < (en + st); i += sp) {
         const color = Math.floor(Math.random() * 360);
         multiTable += `
-            <div class="table" style="background-color: hsl(${color},100%,50%);">
+            <article class="table" style="background-color: hsl(${color},100%,50%);">
                 <h2 class="t-title">${i.toString(base)}</h2>
                 <ul class="multiplication" style="background-color: hsl(${color},100%,70%);">
                     ${generateTables(st,en,sp,base,i)}
                 </ul>
-            </div>`;
+            </article>`;
         
         
     }
@@ -27,4 +29,4 @@ function generateTables(start,end,step,output,startMulti) {
     return multi;
 }
 
-main.innerHTML = generateRow(1,10,1,10)
+main.innerHTML = generateRow(+sta,+end,+ste,+bse)
